5d98a09460e0e7565a6cae209c47bf9aea7aea6a
martin52 <martin52@stolaf.edu>
2021-05-07 10:14:13 -0500

    Working review array - limited functionality

--- Review_Array.cpp
+++ Review_Array.cpp
@@ -11,9 +11,10 @@ class Review {
     int stars;
     char *date;
     char *username;
+    char *meal;
 
 public:
-    Review(int s, char *d, char *u) {
+    Review(int s, char *d, char *u, char *m) {
         cerr << "Review constructor initialized" << endl;
         if (s < 1) {
             stars = 1;
@@ -26,6 +27,7 @@ public:
         }
         int datelen = getlenstring(d);
         int userlen = getlenstring(u);
+        int meallen = getlenstring(m);
         date = new char[datelen];
         for (int i = 0; i < datelen; ++i) {
             date[i] = d[i];
@@ -33,6 +35,10 @@ public:
         username = new char[userlen];
         for (int i = 0; i < userlen; ++i) {
             username[i] = u[i];
+        }
+        meal = new char[meallen];
+        for (int i = 0; i < meallen; ++i) {
+            meal[i] = m[i];
         }
     }
 
@@ -54,12 +60,18 @@ public:
             username[i] = 'x';
         }
         username[8] = 0;
+        meal = new char[8];
+        for (int i = 0; i < 7; ++i) {
+            meal[i] = 'x';
+        }
+        meal[8] = 0;
     }
 
     Review(const Review &input) {
         cerr << "Review copy constructor initialized" << endl;
         int datelen = getlenstring(input.date);
         int userlen = getlenstring(input.username);
+        int meallen = getlenstring(input.meal);
         stars = input.stars;
         date = new char[datelen];
         for (int i = 0; i < datelen; ++i) {
@@ -69,17 +81,23 @@ public:
         for (int i = 0; i < userlen; ++i) {
             username[i] = input.username[i];
         }
+        meal = new char[meallen];
+        for (int i = 0; i < meallen; ++i) {
+            meal[i] = input.meal[i];
+        }
     }
 
     ~Review() {
         cerr << "Review destructor initialzied" << endl;
         delete [] date;
         delete [] username;
+        delete [] meal;
     }
 
     int get_stars() {return stars;}
     char *get_date() {return date;}
     char *get_username() {return username;}
+    char *get_meal() {return meal;}
 
     void set_stars(int s) {stars = s;}
     void set_date(char *d) {
@@ -96,26 +114,60 @@ public:
             username[i] = u[i];
         }
     }
-    void test_display() {cerr << get_stars() << " " << get_date() << " " << get_username() << endl;}
+    void set_meal(char *m) {
+        delete [] meal;
+        meal = new char [getlenstring(m)];
+        for (int i = 0; i < getlenstring(m); ++i) {
+            meal[i] = m[i];
+        }
+    }
+    void test_display() {cerr << get_stars() << " " << get_date() << " " << get_username() << " " << get_meal() << endl;}
 };
 
 class Review_Array {
+    int length;
+    Review *review_array;
+
 public:
-    Review_Array(int length) {
-        //constructor
+    Review_Array(int l) : length(l) {
+        cerr << "Review_Array constructor initialized" << endl;
+        review_array = new Review[length];
     }
 
     Review_Array() {
-        //default constructor
+        cerr << "Review_Array default constructor initialized" << endl;
+        length = 0;
+        review_array = new Review[length];
     }
 
-    Review_Array(const Review_Array & input) {
-        //copy constructor
+    ~Review_Array() {
+        cerr << "Review_Array destructor initialized" << endl;
+        delete [] review_array;
     }
 
-    void add_entry(Review latest) {
+    Review & operator[](int i) {
+        return review_array[i];
+    }
+
+    void edit_entry(int i, Review newdata) {
+        review_array[i].set_stars(newdata.get_stars());
+        review_array[i].set_date(newdata.get_date());
+        review_array[i].set_username(newdata.get_username());
+        review_array[i].set_meal(newdata.get_meal());
+    }
+
+    /*void add_entry(Review latest) {
         //adds new entry to the end of the array by deallocating the old array and copying the contents into a new array one index longer that contains the new entry at the end
-    }
+        Review_Array temp(length + 1);
+        for (int i = 0; i < length; ++i) {
+            temp[i] = review_array[i];
+        }
+        temp[length + 1] = latest;
+        delete [] review_array;
+        Review_Array review_array(length + 1);
+        review_array = temp;
+        delete [] temp;
+    }*/
 
     void delete_entry(int i) {
         //deletes the entry at the corresponding index, deallocates the old array and copies the contents into a new array that's one index shorter, so the index of every entry after the entry that was deleted is decreased by one
@@ -127,28 +179,12 @@ public:
 };
 
 int main() {
-    char username1[] = "martinad52";
-    char username2[] = "guzman6";
-    char username3[] = "xxzghvum69";
-    char date1[] = "1/1/01";
-    char date2[] = "12/6/12";
-    char date3[] = "8/31/20";
-    Review adam_artichokepizza(3, date1, username1);
-    adam_artichokepizza.test_display();
-    Review defaulty_rice;
-    defaulty_rice.test_display();
-    Review copycat_artichokepizza(adam_artichokepizza);
-    copycat_artichokepizza.test_display();
-    adam_artichokepizza.set_stars(4);
-    adam_artichokepizza.set_username(username2);
-    adam_artichokepizza.set_date(date2);
-    adam_artichokepizza.test_display();
-    Review tristan_bratwurst(2, date3, username3);
-    tristan_bratwurst.test_display();
-    Review *review_array = new Review[4];
-    review_array[0] = adam_artichokepizza;
-    review_array[1] = defaulty_rice;
-    review_array[2] = copycat_artichokepizza;
-    review_array[3] = tristan_bratwurst;
-    delete [] review_array;
+    Review newreview(3, "12/12/12", "martin52", "pizza");
+    Review newreview2(2, "2/2/2", "tululu2", "tofu");
+    Review_Array test(4);
+    test[2].test_display();
+    test.edit_entry(2, newreview);
+    test[2].test_display();
+    /*test.add_entry(newreview2);
+    test[2].test_display();*/
 }
