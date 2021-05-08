fee9c59a87d6da3b31d3844866df08f4424bd9db
martin52 <martin52@stolaf.edu>
2021-05-08 17:07:19 -0500

    Fully functional Review_Array class

--- Review_Array.cpp
+++ Review_Array.cpp
@@ -15,7 +15,7 @@ class Review {
 
 public:
     Review(int s, char *d, char *u, char *m) {
-        cerr << "Review constructor initialized" << endl;
+        //cerr << "Review constructor initialized" << endl;
         if (s < 1) {
             stars = 1;
         }
@@ -43,7 +43,7 @@ public:
     }
 
     Review() {
-        cerr << "Review default constructor initialized" << endl;
+        //cerr << "Review default constructor initialized" << endl;
         stars = -1; //NOTE: When implementing this on React, we should make it so that if a star rating is -1, it displays a special message to the effect of "NO REVIEW" or something.
         date = new char[8];
         date[0] = 'X';
@@ -68,7 +68,7 @@ public:
     }
 
     Review(const Review &input) {
-        cerr << "Review copy constructor initialized" << endl;
+        //cerr << "Review copy constructor initialized" << endl;
         int datelen = getlenstring(input.date);
         int userlen = getlenstring(input.username);
         int meallen = getlenstring(input.meal);
@@ -88,7 +88,7 @@ public:
     }
 
     ~Review() {
-        cerr << "Review destructor initialzied" << endl;
+        //cerr << "Review destructor initialzied" << endl;
         delete [] date;
         delete [] username;
         delete [] meal;
@@ -130,18 +130,18 @@ class Review_Array {
 
 public:
     Review_Array(int l) : length(l) {
-        cerr << "Review_Array constructor initialized" << endl;
+        //cerr << "Review_Array constructor initialized" << endl;
         review_array = new Review[length];
     }
 
     Review_Array() {
-        cerr << "Review_Array default constructor initialized" << endl;
+        //cerr << "Review_Array default constructor initialized" << endl;
         length = 0;
         review_array = new Review[length];
     }
 
     ~Review_Array() {
-        cerr << "Review_Array destructor initialized" << endl;
+        //cerr << "Review_Array destructor initialized" << endl;
         delete [] review_array;
     }
 
@@ -156,35 +156,44 @@ public:
         review_array[i].set_meal(newdata.get_meal());
     }
 
-    /*void add_entry(Review latest) {
-        //adds new entry to the end of the array by deallocating the old array and copying the contents into a new array one index longer that contains the new entry at the end
-        Review_Array temp(length + 1);
-        for (int i = 0; i < length; ++i) {
-            temp[i] = review_array[i];
+    void add_entry(Review latest) {
+        int i = length;
+        while (review_array[i - 1].get_stars() == -1) {
+            cerr << i << endl;
+            --i;
         }
-        temp[length + 1] = latest;
-        delete [] review_array;
-        Review_Array review_array(length + 1);
-        review_array = temp;
-        delete [] temp;
-    }*/
+        edit_entry(i, latest);
+    }
 
     void delete_entry(int i) {
-        //deletes the entry at the corresponding index, deallocates the old array and copies the contents into a new array that's one index shorter, so the index of every entry after the entry that was deleted is decreased by one
+        //replaces the contents at a particular index with the default values
+        Review blank;
+        edit_entry(i, blank);
     }
 
     void test_display() {
         //displays the entire contents of the array
+        for (int i = 0; i < length; ++i) {
+            review_array[i].test_display();
+        }
     }
 };
 
 int main() {
     Review newreview(3, "12/12/12", "martin52", "pizza");
     Review newreview2(2, "2/2/2", "tululu2", "tofu");
-    Review_Array test(4);
-    test[2].test_display();
-    test.edit_entry(2, newreview);
-    test[2].test_display();
-    /*test.add_entry(newreview2);
-    test[2].test_display();*/
+    Review newreview3(5, "7/4/15", "hubert9", "olives");
+    Review_Array driver_test(10);
+    cerr << "An uninitialized review_array:" << endl;
+    driver_test.test_display();
+    cerr << "Editing entries:" << endl;
+    driver_test.edit_entry(0, newreview);
+    driver_test.edit_entry(5, newreview2);
+    driver_test.test_display();
+    cerr << "Adding entries to the end of the array:" << endl;
+    driver_test.add_entry(newreview3);
+    driver_test.test_display();
+    cerr << "Deleting entries:" << endl;
+    driver_test.delete_entry(5);
+    driver_test.test_display();
 }
